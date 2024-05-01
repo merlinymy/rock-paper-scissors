205bfcbf476fef60366e9e247680bfe211f0867f
Merlin Yang <lordhorcrux@gmail.com>
2024-05-01 12:48:08 -0500

    add function to get computer choice
    0 1 2 are rock, paper and scissors

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello Rock");
+function getComputerChoice() {
+    // Get a random choice from three possibilities (0, 1, 2)
+    const choice = Math.random() * 3;
+    // return the random choice
+    return choice;
+}
+
