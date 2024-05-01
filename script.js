503d3554b9c45710024c1860c9048cfe37c5900c
Merlin Yang <lordhorcrux@gmail.com>
2024-05-01 12:49:38 -0500

    fix value generation logic
    add Math.floor to the logic

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getComputerChoice() {
     // Get a random choice from three possibilities (0, 1, 2)
-    const choice = Math.random() * 3;
+    const choice = Math.floor(Math.random() * 3);
     // return the random choice
     return choice;
 }
 
+console.log(getComputerChoice());
